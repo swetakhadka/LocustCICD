b6c59f8d2ccffb8bda5a385a8ca7703c4835ac2e
swetakhadka <swetakhadka095@gmail.com>
2024-12-03 16:14:30 +0545

    sevice-comit

--- regres.py
+++ regres.py
@@ -10,3 +10,9 @@ class Myresq(HttpUser):
     @task
     def load_FAQ_page(self):
         self.client.get("/faq")
+
+    @task
+    def load_career_page(self):
+        self.client.get("/career")
+
+
